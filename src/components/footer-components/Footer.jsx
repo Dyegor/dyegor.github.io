bbc0e5882de4d9dd38cbb8752b77f888b1a4981e
Dmitriy <dmitriy.yegorov85@gmail.com>
2021-01-16 12:32:32 +1300

    Added footer components

---
+++ src/components/footer-components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div id="footer">
+            <p class="copyright">
+                <b>© 2020 Dmitriy Yegorov</b>
+            </p>
+        </div>
+    );
+}
