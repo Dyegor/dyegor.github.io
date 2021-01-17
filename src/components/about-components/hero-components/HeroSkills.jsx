df16fa4a991a9d9e56089ceef8921cd2fed524cb
Dmitriy <dmitriy.yegorov85@gmail.com>
2021-01-18 10:43:41 +1300

    Add Hero-components section

---
+++ src/components/about-components/hero-components/HeroSkills.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroTechStackSection from "./HeroTechStackSection";
+
+export default function HeroSkills() {
+    return (
+        <div class="hero-section">
+            <div class="skills-header">
+                <h2>Skills</h2>
+            </div>
+            <HeroTechStackSection />
+        </div>
+    );
+}
