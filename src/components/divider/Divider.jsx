d05ec61a83ad629fd8c4535c651a0d45b8bd021d
Dmitriy <dmitriy.yegorov85@gmail.com>
2021-01-21 19:39:15 +1300

    Add divider element with adjustable width

---
+++ src/components/divider/Divider.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class PortfolioButtonsContainer extends Component {
+    render() {
+        return (
+            <div className="divider" style={this.props.style}/>
+        );
+    }
+}
