161edd3ce7901ee4a4d351c86a8ca617e556671e
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-17 23:54:59 +1300

    Add portfolio page route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from "./components/navbar-components/Navbar";
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 import Portfolio from "./pages/Portfolio";
 import Footer from "./components/footer-components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/portfolio" component={Portfolio} />
-      </Switch>
-      <Footer />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/portfolio" component={Portfolio} />
+        </Switch>
+        <Footer />
     </BrowserRouter>
   );
 }
