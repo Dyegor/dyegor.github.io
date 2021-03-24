c821aa9a1beaf8fdd412e3bd40539fc82e716268
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-24 20:49:23 +1300

    Add Social Icons container component

--- src/components/social-icons-components/SocialIcons.jsx
+++ src/components/social-icons-components/SocialIcons.jsx
@@ -4,12 +4,12 @@ import SocialIcon from "./SocialIcon";
 export default function SocialIcons() {
     const socialIconsItems = [
         {
-            link: "https://www.linkedin.com/in/dmitriy-y-854a8b16a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BwA6JBOOeQkeBWWIZQ4oB1w%3D%3D",
-            iconName: "linkedin"
-        },
-        {
             link: "https://github.com/Dyegor",
             iconName: "github"
+        },
+        {
+            link: "https://www.linkedin.com/in/dmitriy-y-854a8b16a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BwA6JBOOeQkeBWWIZQ4oB1w%3D%3D",
+            iconName: "linkedin"
         },
         {
             link: "https://www.facebook.com/profile.php?id=100001211329207",
@@ -19,7 +19,7 @@ export default function SocialIcons() {
 
 
     return (
-        <div class="social-icons">
+        <div className="social-icons">
             <ul className="list-inline list-social-icons mb-0">
                 {socialIconsItems.map((item, index) => (
                     <SocialIcon
