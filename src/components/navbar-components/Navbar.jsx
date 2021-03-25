faabea06c656f28fc0fb586565ee5134d876abbc
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-25 21:13:29 +1300

    Add Navbar component

--- src/components/navbar-components/Navbar.jsx
+++ src/components/navbar-components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import NavbarImg from "./NavbarImg";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js"
+import "../../../node_modules/jquery/dist/jquery.min.js"
 import Navigation from "./Navigation";
 import Buttons from "../buttons/navbar-buttons/Buttons";
+import NavbarCollapse from "../buttons/navbar-buttons/NavbarCollapse.jsx";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <NavbarImg imgUrl="img/profile.jpg" />
-            <Navigation />
-            <Buttons />
+        <nav className="navbar navbar-expand-md navbar-dark" id="navBar">
+            <div className="container-fluid">
+                <NavbarCollapse />
+                <Navigation />
+                <Buttons />
+            </div>
         </nav>
     );
 }
