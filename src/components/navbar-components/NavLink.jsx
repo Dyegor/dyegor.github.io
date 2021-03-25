c73f169ad0104fdef28dc825e2b9b73daf71fa23
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-25 21:14:13 +1300

    Add NavLink component

--- src/components/navbar-components/NavLink.jsx
+++ src/components/navbar-components/NavLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NavLink(props) {
   return (
     <Link to={props.link}>
-      <li className="nav-item">
+      <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
         <a className="nav-link js-scroll-trigger active" href="/">{props.title}</a>
       </li>
     </Link>
