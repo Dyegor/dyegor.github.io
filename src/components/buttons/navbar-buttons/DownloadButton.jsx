d145902903d7af3a4a3416609f6fd3371e106c6f
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-29 20:48:26 +1300

    Add Download button component

--- src/components/buttons/navbar-buttons/DownloadButton.jsx
+++ src/components/buttons/navbar-buttons/DownloadButton.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+import { Button, makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+    contactButton: {
+        ['@media (max-width:550px)']: {
+            margin: "1em 0"
+        }
+    }
+}));
 
-export default function DownloadButton(props) {
+export default function ContactButton(props) {
+    const classes = useStyles();
+    
     return (
-        <Button variant="contained" color="primary" href={props.buttonHref} download>{props.buttonName}</Button>
+        <Button className={classes.contactButton} variant="outlined" color="primary" href={props.buttonHref} download>{props.buttonName}</Button>
     );
 }
