57e6ef4af704f6a6f6f9a00a109999af2ec96ac5
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-29 20:48:11 +1300

    Add Contact button component

--- src/components/buttons/navbar-buttons/ContactButton.jsx
+++ src/components/buttons/navbar-buttons/ContactButton.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 
-
 export default function ContactButton(props) {
     return (
-        <Button variant="contained" color="primary" href={props.buttonHref}>{props.buttonName}</Button>
+        <Button variant="outlined" color="primary" href={props.buttonHref}>{props.buttonName}</Button>
     );
 }
