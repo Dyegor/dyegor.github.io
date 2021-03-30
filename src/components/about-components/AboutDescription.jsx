c36c73a1eb0e8ed6c1476fbe049b441d756111c0
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-30 21:20:39 +1300

    Add About description section

---
+++ src/components/about-components/AboutDescription.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutDescription() {
+    return (
+        <p className="mb-5 about-description">
+            <b>
+                I have a passion for coding, solving problems, and working on fun but challenging projects as part of a team or solo.
+                I enjoy creating web applications with JavaScript, Typescript, and React.js and do not shy away from new technologies, frameworks, and tools that could enhance or deepen my knowledge and skill set.
+            </b>
+        </p>
+    );
+}
