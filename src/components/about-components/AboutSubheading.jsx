d5627335c9ff1c9140f83fd2ff63985b2e20d465
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-30 21:24:40 +1300

    Add About subheading section

---
+++ src/components/about-components/AboutSubheading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutSubheading() {
+    return (
+        <div className="subheading mb-5">
+            <h3 className="about-heading">Full Stack Web Developer</h3>
+        </div>
+    );
+}
