78d144bfca68aca47da125431933723256db80c3
dyegor <dmitriy.yegorov85@gmail.com>
2021-03-30 21:23:59 +1300

    Add About heading section

---
+++ src/components/about-components/AboutHeading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutHeading() {
+    return (
+        <h1>
+            <span className="text-primary">Dmitriy Yegorov</span>
+        </h1>
+    );
+}
