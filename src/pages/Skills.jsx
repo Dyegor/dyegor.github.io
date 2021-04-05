54f320ab58837742294b984fef3cbb5f87658e3f
dyegor <dmitriy.yegorov85@gmail.com>
2021-04-05 20:09:46 +1200

    Add skills paige

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroSkills from "../components/skills-components/HeroSkills";
+
+export default function Skills() {
+  return (
+    <section className="resume-section p-3 p-lg-5 d-flex flex-column">
+      <HeroSkills />
+    </section>
+  );
+}
