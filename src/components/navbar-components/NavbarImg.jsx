eda490fbc0ab5ed77c6c784b0d5bad7079ebf35f
dyegor <dmitriy.yegorov85@gmail.com>
2021-04-13 22:03:48 +1200

    Remove Navbar Image

--- src/components/navbar-components/NavbarImg.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NavbarImg(props) {
-    return (
-        <span className="navbar-brand js-scroll-trigger d-none d-lg-block">
-            <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={props.imgUrl} alt="navbar-img" />
-        </span>
-    );
-}
