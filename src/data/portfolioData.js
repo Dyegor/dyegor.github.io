3a99196328fbb800ed3fb3ffde06afde03f3462e
dyegor <dmitriy.yegorov85@gmail.com>
2021-04-27 22:18:06 +1200

    Data dir add

---
+++ src/data/portfolioData.js
@@ -0,0 +1,39 @@
+const PortfolioData = [
+    {
+        headerText: "ToDo List App",
+        liveLink: "http://128.199.231.48",
+        gitHubLink: "https://github.com/Dyegor/To-Do-List",
+        imgSrc: "img/portfolio/p-1.jpg",
+        description: "Manage your tasks, save and delete lisits for future use"
+    },
+    {
+        headerText: "Url Shortener",
+        liveLink: "http://54.252.241.169/",
+        gitHubLink: "https://github.com/Dyegor/url-shortener",
+        imgSrc: "img/portfolio/p-2.jpg",
+        description: "Web service, which can get shorter version with a long url and store it in the database"
+    },
+    {
+        headerText: "Wages Counter",
+        liveLink: "http://wagescounter1-env.eba-cmi3brvm.ap-southeast-2.elasticbeanstalk.com/",
+        gitHubLink: "https://github.com/Dyegor/WagesCounter",
+        imgSrc: "img/portfolio/p-4.jpg",
+        description: "Keep track of your weekly working hours. Calculates total income and tax payed."
+    },
+    {
+        headerText: "Random Quote Machine",
+        liveLink: "http://dyegor.github.io/random-quote-machine/",
+        gitHubLink: "https://github.com/Dyegor/random-quote-machine",
+        imgSrc: "img/portfolio/p-3.jpg",
+        description: "My Random Quote Machine app",
+    },
+    {
+        headerText: "This Web Site",
+        liveLink: "http://dyegor.github.io/",
+        gitHubLink: "https://github.com/Dyegor/dyegor.github.io/",
+        imgSrc: "img/portfolio/p-5.jpg",
+        description: "My portfolio website using React"
+    }
+];
+
+export default PortfolioData;
---
+++ src/data/techSkillsData.js
@@ -0,0 +1,52 @@
+const techStackItems = [
+    {
+        title: "HTML5",
+        imgSrc: "img/skills/html.png"
+    },
+    {
+        title: "CSS",
+        imgSrc: "img/skills/css.png"
+    },
+    {
+        title: "JavaScript",
+        imgSrc: "img/skills/javascript.png"
+    },
+    {
+        title: "React",
+        imgSrc: "img/skills/react.png"
+    },
+    {
+        title: "ExpressJs",
+        imgSrc: "img/skills/express.png"
+    },
+    {
+        title: "MySql",
+        imgSrc: "img/skills/mysql.png"
+    },
+    {
+        title: "MongoDB",
+        imgSrc: "img/skills/mongodb.png"
+    },
+    {
+        title: "Babel",
+        imgSrc: "img/skills/babel.png"
+    },
+    {
+        title: "NodeJs",
+        imgSrc: "img/skills/node.png"
+    },
+    {
+        title: "JAVA",
+        imgSrc: "img/skills/java.png"
+    },
+    {
+        title: "Bootstrap",
+        imgSrc: "img/skills/bootstrap.png"
+    },
+    {
+        title: "git",
+        imgSrc: "img/skills/git.png"
+    }
+];
+
+export default techStackItems;
